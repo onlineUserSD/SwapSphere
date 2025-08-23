94dbcde13a11578ba8467d9587a3b58101a4c183
Karan Kumar Nonia <karankumarnonia11@gmail.com>
2025-08-24 01:57:27 +0530

    basic structure

---
+++ backend_swapsphere/src/main/java/com/teamshyt/backend_swapsphere/BackendSwapsphereApplication.java
@@ -0,0 +1,13 @@
+package com.teamshyt.backend_swapsphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSwapsphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSwapsphereApplication.class, args);
+	}
+
+}
---
+++ backend_swapsphere/src/test/java/com/teamshyt/backend_swapsphere/BackendSwapsphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamshyt.backend_swapsphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSwapsphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
