cd7b6ab65c498b837e37a8e9b4b02fd08186467d
Karan Kumar Nonia <karankumarnonia11@gmail.com>
2025-08-25 02:14:01 +0530

    auth filter chaining

---
+++ backend_swapsphere/src/main/java/com/teamshyt/service/JwtAuthFilter.java
@@ -0,0 +1,59 @@
+package com.teamshyt.service;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.teamshyt.model.User;
+import com.teamshyt.repo.UserRepository;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthFilter extends OncePerRequestFilter {
+    private final JwtUtil jwtUtil;
+    private final UserRepository urepo;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        final String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        final String token = authHeader.substring(7); // "Bearer " = index - 0 to 6 so token starts from 7th index
+
+        if (!jwtUtil.validateToken(token)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String email = jwtUtil.getEmailFromToken(token);
+        String roleString = jwtUtil.getRoleFromToken(token);
+
+        Optional<User> optional = urepo.findByEmail(email);
+        if (optional.isPresent()) {
+            var authority = new SimpleGrantedAuthority("ROLE_" + roleString); // it will be like - ROLE_USER or
+                                                                              // ROLE_ADMIN
+            var auth = new UsernamePasswordAuthenticationToken(email, null, List.of(authority));
+            SecurityContextHolder.getContext().setAuthentication(auth);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
