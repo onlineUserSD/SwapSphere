f271b31ac8d22a32dc66401eb6ae030b3779a3f7
Karan Kumar Nonia <karankumarnonia11@gmail.com>
2025-08-25 02:01:48 +0530

    added UserRole Enum - ADMIN/USER

---
+++ backend_swapsphere/src/main/java/com/teamshyt/model/UserRole.java
@@ -0,0 +1,10 @@
+package com.teamshyt.model;
+
+public enum UserRole {
+    ADMIN,
+    USER
+}
+
+// normal user - user role
+// admin for managing others like getting all active users data and can do
+// manage them
