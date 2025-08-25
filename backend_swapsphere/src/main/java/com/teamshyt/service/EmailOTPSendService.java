fff95212d738d52d97b2ad8cebe45ac05f15c015
Karan Kumar Nonia <karankumarnonia11@gmail.com>
2025-08-25 15:24:08 +0530

    otp

---
+++ backend_swapsphere/src/main/java/com/teamshyt/service/EmailOTPSendService.java
@@ -0,0 +1,27 @@
+package com.teamshyt.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailOTPSendService {
+    private final JavaMailSender javaMailSender;
+
+    @Value("${app.otp.expiry.minutes}")
+    private int otpExpiryMinutes;
+
+    public EmailOTPSendService(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
+    public void sendOtp(String to, String subject, String otp) {
+        SimpleMailMessage msg = new SimpleMailMessage();
+        msg.setTo(to);
+        msg.setSubject(subject);
+        msg.setText("Your Verification code is : " + otp + "\nThis code is valid for " + otpExpiryMinutes
+                + " minutes only.");
+        javaMailSender.send(msg);
+    }
+}
---
+++ backend_swapsphere/src/main/java/com/teamshyt/service/EmailService.java
@@ -0,0 +1,5 @@
+package com.teamshyt.service;
+
+public class EmailService {
+
+}
