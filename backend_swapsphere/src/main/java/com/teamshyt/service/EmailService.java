73a10b2c9f844242865b68ad6e5f1bb7a6b57145
Karan Kumar Nonia <karankumarnonia11@gmail.com>
2025-08-26 02:39:07 +0530

    added time gap between otps

--- backend_swapsphere/src/main/java/com/teamshyt/service/EmailService.java
+++ backend_swapsphere/src/main/java/com/teamshyt/service/EmailService.java
@@ -29,6 +29,9 @@ public class EmailService {
     @Value("${OTP_EXPIRY}")
     private int otpExpiryMinutes;
 
+    @Value("${OTP_RESEND_GAP_IN_SECONDS}")
+    private long otpResendGapinSeconds;
+
     public String generateOtp() {
         int lowValue = (int) Math.pow(10, Math.max(1, otpLength - 1));
         int maxValue = (int) Math.pow(10, otpLength) - 1;
@@ -45,6 +48,7 @@ public class EmailService {
         emailObject.setType(type);
         emailObject.setExpiryTime(LocalDateTime.now().plusMinutes(otpExpiryMinutes));
         emailObject.setConsumed(false);
+        emailObject.setCreatedAt(LocalDateTime.now()); // from now otp will have creation time for tracking
 
         emailRepository.save(emailObject);
 
@@ -73,6 +77,38 @@ public class EmailService {
 
     @Transactional
     public void resendOtp(String email, VerificationType type) {
+        // adding check to time gap between otps 
+        Optional<Email> latestOtp = emailRepository.findFirstByEmailAndTypeOrderByExpiryTimeDesc(email, type);
+        if (latestOtp.isPresent()) {
+            LocalDateTime lastRequestedTime = latestOtp.get().getCreatedAt();
+
+            if (lastRequestedTime.isAfter(LocalDateTime.now().minusSeconds(otpResendGapinSeconds))) {
+                throw new RuntimeException("Please wait before requesting a new OTP. Try again after " + otpResendGapinSeconds + " seconds.");
+            }
+        }
+
+        // it will delete the old otps of the email with the type of request -- VERIFY
+        // OR RESET
+        // here if a user requests 2 or more otps within a small time then there may be
+        // multiple active otps so this method will make sure that there exist only one
+        // valid so it will first delelte the current otp for this email and then create
+        // a new one and send
+        emailRepository.deleteByEmailAndType(email, type);
+
+        // now just create a new otp and sent to email just like the previous method -
+        // createAndSendOtp - just call it with email and type
         createAndSendOtp(email, type);
+
+        // now deleting the used ones - it is like cleaning the DB by removing all useds
+        // or expired otps of any types
+        // here it uses a defined query from EmailRepository - deleteExpiredorConsumed()
+        emailRepository.deleteExpiredorConsumed(LocalDateTime.now());
+        // agar otp ka expiry time abhi ke time se pehle ka h to wo delete hoga
+
+        // here the first method will just delete the current type otps whcih means if a
+        // request type is not requested from so long then its old used and expired otps
+        // may be present in the DB
+        // so this 2nd delete method ensure that all useds or expired otps will be
+        // globally cleanedUp keeping DB light and clean.
     }
 }
