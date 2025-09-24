b6e084274325df4e0555b94181ebc8220f5840ed
onlineUserSD <tzsaad6@gmail.com>
2025-09-24 20:59:45 +0530

    Updated model classes

--- backend_swapsphere/src/main/java/com/teamshyt/model/Category.java
+++ backend_swapsphere/src/main/java/com/teamshyt/model/Category.java
@@ -1,4 +1,21 @@
 package com.teamshyt.model;
 
-public class category {
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "category_item")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String category_name;  //Products will be divided into categories like CLOTHES,ELECTRONICS,
+    // SPORTS,SHOES etc (will be updated later with more details).
+
 }
--- backend_swapsphere/src/main/java/com/teamshyt/model/Exchange.java
+++ backend_swapsphere/src/main/java/com/teamshyt/model/Exchange.java
@@ -1,4 +1,43 @@
 package com.teamshyt.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "exchange_offers")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Exchange {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "targetProduct_id", nullable = false)
+    private Product targetProduct;
+
+    @ManyToOne
+    @JoinColumn(name = "offeredProduct_id",nullable = false)
+    private Product offeredProduct;
+
+    @ManyToOne
+    @JoinColumn(name = "fromUser_id",nullable = false)
+    private User fromUser;
+
+    @ManyToOne
+    @JoinColumn(name = "toUser_id",nullable = false)
+    private User toUser;
+
+    @Enumerated(EnumType.STRING)
+    private OffereStatus status;
+
+    private Date proposedAt;
+
 }
