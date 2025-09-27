45b3aa0e591a5b2a0dbba984da65cef2d11c1e69
onlineUserSD <tzsaad6@gmail.com>
2025-09-27 18:17:02 +0530

    Created Service layer of Prodcut

---
+++ backend_swapsphere/src/main/java/com/teamshyt/service/ProductService.java
@@ -0,0 +1,16 @@
+package com.teamshyt.service;
+
+import com.teamshyt.dto.ProductRequest;
+import com.teamshyt.dto.ProductResponse;
+import com.teamshyt.model.User;
+
+import java.util.List;
+
+public interface ProductService {
+    ProductResponse addProduct(ProductRequest request);
+    ProductResponse updateProduct(Long id, ProductRequest request);
+    void deleteProduct(Long id);
+    ProductResponse getProductById(Long id);
+    List<ProductResponse> getProductByUser(User user);
+    List<ProductResponse> getAllProduct();
+}
